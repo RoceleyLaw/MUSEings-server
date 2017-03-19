42f84e3340e75adcbca345aac44db1fa5bc29ab0
roceleylaw <roceleylaw@gmail.com>
2017-03-19 04:48:33 -0400

    adjustment

--- api/models/museings-api-model.js
+++ api/models/museings-api-model.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  _id: { type:String }
+  _id: { type: String },
   username: { type: String, required: true },
   data: { type: String },
   audio: { type: String }
