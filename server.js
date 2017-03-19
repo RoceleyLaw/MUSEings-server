a20a47e4b560dcce9b2077fd11175cd466fbf32d
roceleylaw <roceleylaw@gmail.com>
2017-03-19 03:21:29 -0400

    access modification

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.setHeader('Access-Control-Allow-Origin', 'https://obscure-oasis-37920.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
