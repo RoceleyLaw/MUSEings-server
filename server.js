f02bb15d1ba51e2fd74fc8a197046cb45d2be355
roceleylaw <roceleylaw@gmail.com>
2017-03-19 02:19:48 -0400

    changed auth url

--- server.js
+++ server.js
@@ -5,29 +5,30 @@ var mongoose = require('mongoose');
 var User = require('./api/models/museings-api-model');
 var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
+//mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost/userdb');
 mongoose.connect('mongodb://roceleylaw:museings1234@ds135700.mlab.com:35700/museings');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(function (req, res, next) {
-//
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-//
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//
-//     // Pass to next layer of middleware
-//     next();
-// });
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'https://obscure-oasis-37920.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
 var routes = require('./api/routes/museings-api-routes');
