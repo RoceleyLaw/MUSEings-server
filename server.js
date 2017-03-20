e4aec2be4fb2f76aed08ea50237671651f55cae3
roceleylaw <roceleylaw@gmail.com>
2017-03-20 09:36:51 -0400

    allow access for github pages

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'https://roceleylaw.github.io/MUSEings-web-app/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
